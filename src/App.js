544c675c57d93be987f0677207a2d3de04b1bd70
Matthew Rapati <mattrapati@gmail.com>
2020-05-20 17:01:42 -0400

    App now keeps events in state, and a button has been added to generate a new set of events. Custom properties for colours added for a global app theme.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/App.css";
 import DayCalendar from "./components/DayCalendar";
 import { getRandomEvents } from "./lib/utils";
 
 function App() {
-  const events = getRandomEvents(10);
+  const [events, setEvents] = useState(getRandomEvents(10));
 
   return (
     <div className="App">
       <DayCalendar events={events} />
+      <button onClick={() => setEvents(getRandomEvents(10))}>Generate New Events</button>
     </div>
   );
 }
